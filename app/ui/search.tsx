5cf7b73e650ccde60c6229f9fe857d234058d94d
bebusl <bebus1998@naver.com>
2025-12-04 08:32:49 +0900

    URL searchparam 기반 페이지네이션 기능 추가

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,16 +7,18 @@ import { useRouter } from "next/router";
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const { replace } = useRouter();
+  // const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+
+    params.set("page", "1");
 
     if (term) {
       params.set("query", term);
     } else params.delete("query");
 
-    replace(`${pathname}?${params.toString()}`);
+    // replace(`${pathname}?${params.toString()}`);
   }
 
   return (
